1e355b8e4c5523c58f38e4f8db6d5e4c5a191c02
Nguyễn Cao Nguyên <nguyencaonguyen0944322545@gmail.com>
2022-11-30 11:32:53 +0700

    v0.2.2, no previous commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import node_pkg from '../package.json';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const client_pkg = node_pkg;
+    return (
+        <div className="App">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Edit <code>src/App.js</code> and save to reload.<br></br>
+                Oh so this is ReactJS, interesting...<br></br>
+                Go back to main site: <a className="App-link" href="https://choomai.tk">choomai.tk</a>
+            </p>
+            Package: <code>{client_pkg.name}@{client_pkg.version}</code>
+            <a className="App-link" href="https://reactjs.org" target="_blank" rel="noreferrer">Learn React</a>
+        </div>
+    );
 }
-
 export default App;
